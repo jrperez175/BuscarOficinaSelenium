d42f6358e18e07c483ae5c42245e335323bbd858
Jhon Perez <jrperez175@gmail.com>
2018-03-05 16:43:11 -0500

    Coomit 05marzo2018 Segundo Commit

--- src/automationFramework2/FirstTestCase.java
+++ src/automationFramework2/FirstTestCase.java
@@ -1,25 +1,34 @@
 package automationFramework2;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 
 public class FirstTestCase {
 
 public static void main(String[] args) throws InterruptedException {
-		
+		String url="http://www.store.demoqa.com";
+	
+		//FIREFOX
 		//Desde Selenium 3.0 en adelante, necesitamos proporcionar explícitamente el controlador de firefox (geckodriver) 
 		//ya que Firefox ya no proporciona el controlador incorporado por defecto.
 		
-		//System.setProperty("webdriver.gecko.driver", "D:/selenium-java-3.9.1/geckodriver.exe");
-		System.setProperty("webdriver.firefox.marionette", "./geckodriver.exe");
+		//System.setProperty("webdriver.firefox.marionette", "./geckodriver.exe");
 		
+		//Google Drive
+		String exePath = "D:\\chromedriver\\chromedriver.exe";
+		System.setProperty("webdriver.chrome.driver", exePath);
+		
+	
 		// Create a new instance of the Firefox driver
-		WebDriver driver = new FirefoxDriver();
+		//WebDriver driver = new FirefoxDriver();
+		WebDriver driver = new ChromeDriver();
 		
         //Launch the Online Store Website
-		driver.get("http://www.store.demoqa.com");
- 
+		driver.get(url);
+		
+		 
         // Print a Log In message to the screen
         System.out.println("Successfully opened the website www.Store.Demoqa.com");
  
@@ -29,4 +38,5 @@ public static void main(String[] args) throws InterruptedException {
         // Close the driver
         driver.quit();
 
+}
 }
