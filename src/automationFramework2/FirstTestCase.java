aa226a30c8b8e4aed3cbfb84fee799b63506dd36
Jhon Perez <jrperez175@gmail.com>
2018-03-05 20:00:21 -0500

    Commit 05Marzo2018 20:00

--- src/automationFramework2/FirstTestCase.java
+++ src/automationFramework2/FirstTestCase.java
@@ -1,6 +1,8 @@
 package automationFramework2;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
@@ -8,7 +10,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 public class FirstTestCase {
 
 public static void main(String[] args) throws InterruptedException {
-		String url="http://www.store.demoqa.com";
+		String url="http://www.bancolombia.com.co";
 	
 		//FIREFOX
 		//Desde Selenium 3.0 en adelante, necesitamos proporcionar explícitamente el controlador de firefox (geckodriver) 
@@ -17,8 +19,10 @@ public static void main(String[] args) throws InterruptedException {
 		//System.setProperty("webdriver.firefox.marionette", "./geckodriver.exe");
 		
 		//Google Drive
-		String exePath = "D:\\chromedriver\\chromedriver.exe";
-		System.setProperty("webdriver.chrome.driver", exePath);
+		String exePathOficina = "D:\\chromedriver\\chromedriver.exe";
+		String exePathCasa = "H:\\Software\\Automatizacion\\chromedriver_win32\\chromedriver.exe";
+		
+		System.setProperty("webdriver.chrome.driver", exePathCasa);
 		
 	
 		// Create a new instance of the Firefox driver
@@ -28,12 +32,30 @@ public static void main(String[] args) throws InterruptedException {
         //Launch the Online Store Website
 		driver.get(url);
 		
+		 //Hacer click en la imagen visitanos
+		
+		
+		driver.findElement(By.cssSelector("a[title=\"Visítanos\"]")).click();
+		
+		
+		 // Buscar Elmento ubicacion
+		WebElement ubicacion = driver.findElement(By.id("srch-term"));
+		
+		//Digitar informacion
+		ubicacion.sendKeys("Cra. 52 #14-30, Medellín, Antioquia, Colombia");
+		
+		//Enviar informacion
+		//this.ubicacion.submit();
 		 
+		 
+		 
+				
+				
         // Print a Log In message to the screen
         System.out.println("Successfully opened the website www.Store.Demoqa.com");
  
 		//Wait for 5 Sec
-		Thread.sleep(5000);
+		Thread.sleep(1000);
 		
         // Close the driver
         driver.quit();
