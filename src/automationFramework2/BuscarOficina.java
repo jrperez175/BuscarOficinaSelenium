f22ff276a981706072a4324eceb47f2cf92cf057
Jhon Perez <jrperez175@gmail.com>
2018-03-06 19:15:30 -0500

    Commit 06Marzo2018 19:15

--- src/automationFramework2/BuscarOficina.java
+++ src/automationFramework2/BuscarOficina.java
@@ -1,7 +1,7 @@
 package automationFramework2;
 
 import org.openqa.selenium.By;
-//import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
